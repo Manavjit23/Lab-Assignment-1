0f82ab207b4b46c0a6d94c8bc3245d6078d82e5b
Manavjit23 <singhmanavjit100@gmail.com>
2025-09-01 21:50:36 +0530

    Update Q2.cpp

--- Lab Assignment-1/Q2.cpp
+++ Lab Assignment-1/Q2.cpp
@@ -1,38 +1,35 @@
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 
-int main()
-{
-    int n, arr[100], index;
-
-    cout << "Enter the size of the array: ";
+int main() {
+    int n;
+    cout << "Enter size of array: ";
     cin >> n;
 
-    cout << "Enter " << n << " elements:\n";
-    for (int i = 0; i < n; i++)
-    {
+    int arr[100];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    cout << "Enter index to remove: ";
-    cin >> index;
+    
+    sort(arr, arr + n);
 
-    if (index < 0 || index >= n)
-    {
-        cout << "Invalid index!\n";
-        return 0;
+    
+    int i = 0; 
+    for (int j = 1; j < n; j++) {
+        if (arr[j] != arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
     }
 
-    for (int i = index; i < n - 1; i++)
-    {
-        arr[i] = arr[i + 1];
-    }
-    n--;
+    int newSize = i + 1;
 
-    cout << "The array after removing element is: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+    cout << "Array after removing duplicates: ";
+    for (int k = 0; k < newSize; k++) {
+        cout << arr[k] << " ";
     }
     cout << endl;
 
